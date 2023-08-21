d6d3f57c97962330fb883a0bfd7875f11b2256f0
jperezalv23 <jperezalv@unal.edu.co>
2023-08-21 14:52:02 -0500

    Clases Asiento, Motor y Auto
    Creacion de estas clses con sus respectivos metodos.

---
+++ main.py
@@ -0,0 +1,49 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro 
+    def cambiarColor(self, color):
+        self.color = color
+
+    
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro 
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo(self, tipo):
+        self.tipo = tipo
+
+
+class Auto:
+    numinstancias = 0
+    def __init__(self, modelo, precio, marca, registro):
+        Auto.numinstancias += 1
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = [Asiento(self)] 
+        self.marca = marca
+        self.motor = Motor(self)
+        self.registro = registro
+        self.cantidadCreados = Auto.numinstancias
+
+    def cantidadAsientos(self, asientos):
+        return len(self.asientos)
+    
+    def verificarIntegridad(self, asientos, motor):
+        if self.motor.registro() == self.registro:
+            for i in range(len(self.asientos)):
+                if self.asientos[i].registro != self.registro:
+                    return "Las piezas no son originales"
+                else: "Auto original"
+        else: 
+            return "Las piezas no son originales"
+
+    
+    
+
+
+        
